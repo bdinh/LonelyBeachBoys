dbbaaac44780b340c3362dc249026c2d9863d74a
bdinh <baodinh96@gmail.com>
2018-12-05 10:21:10 -0800

    Updates

---
+++ precache-manifest.cadc476d1bcc675e6df0a6d441a90bb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d478be7e2225280f43",
+    "url": "/LonelyBeachBoys/static/js/runtime~main.42d478be.js"
+  },
+  {
+    "revision": "0b026d2dedc31d51dbcb",
+    "url": "/LonelyBeachBoys/static/js/main.0b026d2d.chunk.js"
+  },
+  {
+    "revision": "c4e45eda8f843389e8f6",
+    "url": "/LonelyBeachBoys/static/js/1.c4e45eda.chunk.js"
+  },
+  {
+    "revision": "0b026d2dedc31d51dbcb",
+    "url": "/LonelyBeachBoys/static/css/main.dcce2d95.chunk.css"
+  },
+  {
+    "revision": "ad5968225d57d6e59b0ad0ddc2287ed9",
+    "url": "/LonelyBeachBoys/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hops-n-props/precache-manifest.50537260b506964ee377bbdcd70d20f8.js"
+  "/LonelyBeachBoys/precache-manifest.cadc476d1bcc675e6df0a6d441a90bb9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hops-n-props/index.html", {
+workbox.routing.registerNavigationRoute("/LonelyBeachBoys/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
