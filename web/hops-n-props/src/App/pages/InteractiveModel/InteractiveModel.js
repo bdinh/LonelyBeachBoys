2accb368d3764ab641cb975e23ce3849e547aed6
Sam Otto <moffotto@D-10-19-95-38.dhcp4.washington.edu>
2018-12-04 16:52:42 -0800

    Worked on page and model

--- web/hops-n-props/src/App/pages/InteractiveModel/InteractiveModel.js
+++ web/hops-n-props/src/App/pages/InteractiveModel/InteractiveModel.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react';
 import './InteractiveModel.css';
+import { Form, FormGroup, ControlLabel, FormControl, Row, Col } from 'react-bootstrap';
 
 export class InteractiveModel extends Component {
 
     render() {
         return (
             <div>
-                Interactive Model
+                <Form horizontal>
+                    <Row className="show-grid">
+                        <Col xs={6}>
+                            <FormGroup controlId="formHorizontalEmail">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Email
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="email" placeholder="Email" />
+                                </Col>
+                            </FormGroup>
+                        </Col>
+                        <Col xs={6}>
+                            <FormGroup controlId="formHorizontalPassword">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Password
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="password" placeholder="Password" />
+                                </Col>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                    <Row className="show-grid">
+                        <Col xs={6}>
+                            <FormGroup controlId="formHorizontalEmail">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Email
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="email" placeholder="Email" />
+                                </Col>
+                            </FormGroup>
+                        </Col>
+                        <Col xs={6}>
+                            <FormGroup controlId="formHorizontalPassword">
+                                <Col componentClass={ControlLabel} sm={2}>
+                                    Password
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="password" placeholder="Password" />
+                                </Col>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+
+                </Form>
             </div>
         );
     }
